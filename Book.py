226ac883551e5ebd5ba5dbf73f9ffee55343250f
ethwheel <156877044+ethwheel@users.noreply.github.com>
2025-02-08 14:54:27 -0500

    Add files via upload

---
+++ Book.py
@@ -0,0 +1,7 @@
+# Defines what a Book is and its attributes
+class Book: 
+    def __init__(self,title,isbn,desc):
+        self.title = title
+        self.isbn = isbn
+        self.desc = desc
+    
