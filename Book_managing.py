10f6c0d23dabbeb31ab0bf20ba143e9843c453f3
eviltetracycline <borr24@ivytech.edu>
2025-02-16 21:34:30 -0500

    fixed duplicate file

--- Book_managing.py
+++ Book_managing.py
@@ -1,19 +1,34 @@
-#initialize db funciton creates db if one doesnt exist
 import sqlite3
+#add a book to the database
+def add_book(title, author, isbn):
+    conn = sqlite3.connect("library.db")
+    cursor = conn.cursor()
+    #inserting the book into the db
+    try:
+        cursor.execute("INSERT INTO books (title, author, isbn) VALUES (?, ?, ?)", 
+                       (title, author, isbn))
+        conn.commit()
+        print("Book added successfully!")
+        #each ISBN should(?) be unique, if 2 exist there is a problem(?)
+    except sqlite3.IntegrityError:
+        print("Error: ISBN already exists.")
+    
+    conn.close()
 
-def initialize_database():
-    conn = sqlite3.connect("books.db")
+#delete a book from the database
+#Im doing ISBN because im not sure how I would delete with the title
+def delete_book(isbn):
+    conn = sqlite3.connect("library.db")
     cursor = conn.cursor()
     
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS books (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        author TEXT NOT NULL,
-        isbn TEXT UNIQUE NOT NULL,
-        available INTEGER DEFAULT 1
-    )
-    """)
-    
+    cursor.execute("DELETE FROM books WHERE isbn = ?", (isbn))
+    #checking if we deleted something
+    affected_rows = cursor.rowcount
+
+    if affected_rows > 0:
+        print("Book successfully deleted.")
+    else:
+        print("No book found.")
+
     conn.commit()
     conn.close()
