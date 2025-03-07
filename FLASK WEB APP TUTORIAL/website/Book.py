ac66e4a46d70e85ac29b93508104f1e553bd3f6e
AscensionXD <nhughes66@ivytech.edu>
2025-03-07 17:08:22 -0500

    removed note functionality

---
+++ FLASK WEB APP TUTORIAL/website/Book.py
@@ -0,0 +1,8 @@
+# Defines what a Book is and its attributes
+class Book: 
+    def __init__(self,title,author,isbn,availability):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.availability = availability
+    
